0b621cebcb53558fd1bf1948faa3b02391d98817
Fehmi Anaç <fehmianac@gmail.com>
2024-06-05 11:59:42 +0300

    swagger fix.

--- src/Api/Endpoints/V1/Model/ConfigRequestModel.cs
+++ src/Api/Endpoints/V1/Model/ConfigRequestModel.cs
@@ -4,13 +4,13 @@ namespace Api.Endpoints.V1.Model;
 
 public class ConfigRequestModel
 {
-    public List<ItemConfigWorkingHourModel> WorkingHours { get; set; } = new();
+    public List<ItemConfigWorkingHourRequestModel> WorkingHours { get; set; } = new();
 
     public int SlotCountAtSameTime { get; set; }
 
     public int DurationMinutes { get; set; } = 30;
     
-    public class ItemConfigWorkingHourModel
+    public class ItemConfigWorkingHourRequestModel
     {
         public DayOfWeek DayOfWeek { get; set; } = default!;
         public string Open { get; set; } = default!;
